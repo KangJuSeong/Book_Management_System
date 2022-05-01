54c0b9f0c9b93f54eba41d027cb6747efc405cc7
KangJuSeong <wntjd3686@gmail.com>
2022-05-01 17:10:38 +0900

    modify Book and User class, add Manager class

--- Book.py
+++ Book.py
@@ -1,22 +1,23 @@
 class Book:
     def __init__(self, name: str, author: str, isbn: int, rentaling: bool, location: str):
-        self.name: str = name
-        self.author: str = author
-        self.isbn: int = isbn
-        self.rentaling: bool = rentaling
-        self.location: str = location
+        __self.name: str = name
+        __self.author: str = author
+        __self.isbn: int = isbn
+        __self.rentaling: bool = rentaling
+        __self.location: str = location
 
     def getName(self) -> str:
-        return self.name
+        return __self.name
 
     def getAuthor(self) -> str:
-        return self.author
+        return __self.author
 
     def getIsbn(self) -> int:
-        return self.isbn
+        return __self.isbn
 
     def getRentaling(self) -> bool:
-        return self.rentaling
+        return __self.rentaling
 
     def getLocation(self) -> str:
-        return self.location
+        return __self.location
+
